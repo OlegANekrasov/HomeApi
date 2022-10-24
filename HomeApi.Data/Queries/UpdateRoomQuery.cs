28d168c7cd8bc77c829899ade351a3a04c1f57b5
Oleg Nekrasov <79853886592@yandex.ru>
2022-10-24 14:12:29 +0300

    Добавьте файлы проекта.

---
+++ HomeApi.Data/Queries/UpdateRoomQuery.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeApi.Data.Queries
+{
+    /// <summary>
+    ///  Класс для передачи дополнительных параметров при обновлении комнаты
+    /// </summary>
+    public class UpdateRoomQuery
+    {
+        public string NewName { get; }
+        public int? NewArea { get; }
+        public bool? NewGasConnected { get; }
+        public int? NewVoltage { get; }
+
+        public UpdateRoomQuery(string newName = null, int? newArea = null, bool? newGasConnected = null, int? newVoltage = null)
+        {
+            NewName = newName;
+            NewArea = newArea;
+            NewGasConnected = newGasConnected;
+            NewVoltage = newVoltage;
+        }
+    }
+}
---
+++ HomeApi/Controllers/RoomsController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Threading.Tasks;
+using AutoMapper;
+using HomeApi.Contracts.Models.Devices;
+using HomeApi.Contracts.Models.Rooms;
+using HomeApi.Data.Models;
+using HomeApi.Data.Repos;
+using HomeApi.Data.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeApi.Controllers
+{
+    /// <summary>
+    /// Контроллер комнат
+    /// </summary>
+    [ApiController]
+    [Route("[controller]")]
+    public class RoomsController : ControllerBase
+    {
+        private IRoomRepository _repository;
+        private IDeviceRepository _devices;
+        private IMapper _mapper;
+        
+        public RoomsController(IRoomRepository repository, IMapper mapper, IDeviceRepository devices)
+        {
+            _repository = repository;
+            _mapper = mapper;
+            _devices = devices;
+        }
+
+        //TODO: Задание - добавить метод на получение всех существующих комнат
+
+        /// <summary>
+        /// Просмотр всех существующих комнат
+        /// </summary>
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> GetRooms()
+        {
+            var rooms = await _repository.GetRooms();
+
+            var resp = new GetRoomsResponse 
+            {
+                RoomAmount = rooms.Length,
+                Rooms = _mapper.Map<Room[], RoomView[]>(rooms)
+            };
+
+            return StatusCode(200, resp);
+        }
+
+        /// <summary>
+        /// Обновление существующего устройства
+        /// </summary>
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> Edit(
+            [FromRoute] Guid id,
+            [FromBody] EditRoomRequest request)
+        {
+            var room = await _repository.GetRoomById(id);
+            if (room == null)
+                return StatusCode(400, $"Ошибка: Комната с идентификатором {id} не существует.");
+
+            var oldName = room.Name;
+
+            await _repository.UpdateRoom(room, 
+                new UpdateRoomQuery(request.NewName, request.NewArea, request.NewGasConnected, request.NewVoltage));
+
+            if (oldName != room.Name)
+                await _devices.UpdateDeviceRoom(room);
+
+            return StatusCode(200, $"Комната обновлена! Имя - {room.Name}, " +
+                                   $"Площадь - {room.Area},  " +
+                                   $"Газ подключен - {room.GasConnected}, " +
+                                   $"Напряжение - {room.Voltage}"
+            );
+        }
+
+
+        /// <summary>
+        /// Добавление комнаты
+        /// </summary>
+        [HttpPost] 
+        [Route("")] 
+        public async Task<IActionResult> Add([FromBody] AddRoomRequest request)
+        {
+            var existingRoom = await _repository.GetRoomByName(request.Name);
+            if (existingRoom == null)
+            {
+                var newRoom = _mapper.Map<AddRoomRequest, Room>(request);
+                await _repository.AddRoom(newRoom);
+                return StatusCode(201, $"Комната {request.Name} добавлена!");
+            }
+            
+            return StatusCode(409, $"Ошибка: Комната {request.Name} уже существует.");
+        }
+    }
+}
